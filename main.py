4e1dce121f23f02dc86bd7d528ad2c4a01b29b36
ziliac <cheziliang@gmai.com>
2022-03-27 01:25:20 -0500

    u

--- main.py
+++ main.py
@@ -2,26 +2,20 @@ from flask import Flask, render_template, request, send_file, current_app, url_f
 import numpy as np
 from PIL import Image, ImageFilter, ImageDraw, ImageFont
 from io import BytesIO
-import sys
+import sys, os
 sys.path.insert(0, "../..")
 from libnft.url.request import Asset
 from libnft.utils import *
 from apps import wallpaper
+from base64 import b64encode
 #from libnft.test import data_path
-
+#CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
 
 app = Flask(__name__)
+cache = {
 
-@app.route("/")
-def index():
-    return "<h1> aaa <h>"
+}
 
-@app.route("/integer/<i>")
-def integer(i):
-    i = "something"
-    print(i)
-    #return render_template("test.html", idx=i)
-    return f"<h> {i} </h>"
 
 @app.route("/wallpaper", methods=["POST", "GET"])
 def wallpaper_index():
@@ -37,66 +31,76 @@ def wallpaper_index():
     return render_template("wallpaper_index.html")
 
 
+@app.route("/wallpaper/<slug>/<idx>/<ratio>/img")
+def wallpaper_img(slug, idx, ratio):
+
+    h, w = ratio.split("by")
+    ratio = float(h) / float(w)
+    cache_key = (slug, idx, ratio)
+    log.info(f"cached items: {cache.keys()}, cache id: {id(cache)}")
+    if  cache_key in cache:
+        log.info(f"retreiving image {cache_key} from cache....")
+        new_img = cache[(slug, idx, ratio)]
+    else:
+        log.info(f"image {cache_key} not found in cache, creating new ....")
+        if len(cache) > 50:
+            log.info(f"cache too large btw, clearing...")
+            cache.clear()
+        asset = Asset(slug=slug, idx=idx)
+
+        collection_info = wallpaper.get_info(slug)
+        
+        old_img_bytes = asset.get_img()
+        old_img = Image.open(BytesIO(old_img_bytes))
+        if old_img.size[0] > 1000:
+            old_img = old_img.resize((1000, 1000))
+
+        log.info(f"processing image")
+        old_w, old_h = old_img.size
+        new_w = old_w
+        new_h = int(new_w * ratio)
+        bg_color = old_img.getpixel((10, 10))
+        new_img = Image.new("RGBA", size=(new_w, new_h), color=bg_color)
+        paste_anchor = (0, new_h - old_h)
+        new_img.paste(old_img, paste_anchor)
+
+        # write txt
+        log.info(f"done processing image")
+        font_path = Path(current_app.root_path) / "static" / "fonts" / "AbrilFatface-Regular.ttf"
+        font = ImageFont.truetype(str(font_path), 200)
+        txt = Image.new('RGBA', new_img.size, (255,255,255,0))
+        drawer = ImageDraw.Draw(new_img)    
+        drawer.text((new_w // 2, (new_h - old_h) // 2), 
+                    collection_info["title"],
+                    fill=(0, 0, 0, 255), font=font, anchor="mm")
+        new_img = Image.alpha_composite(new_img, txt)
+
+        log.info(f"saving {cache_key} to cache")
+        cache[(slug, idx, ratio)] = new_img
+        log.info(f"cached items: {cache.keys()}, cache id: {id(cache)}")
+
+        # ...
+    image_io = BytesIO()
+    new_img.save(image_io, format='PNG')
+    image_io.seek(0)
+
+
+    return send_file(
+        image_io,
+        as_attachment=False,
+        mimetype='image/png'
+    )
+
+
 @app.route("/wallpaper/<slug>/<idx>")
 def wallpaper_out(slug, idx):
-    #from PIL import Image
-    #from io import BytesIO
-    asset = Asset(slug=slug, idx=idx)
     size = request.args.get("size")
     h, w = size.split("(")[0].split(":")
-    ratio = float(h) / float(w)
-
-
-    collection_info = wallpaper.get_info(slug)
-    
-    old_img_url = asset.img_url
-    old_img_bytes = asset.get_img()
-    old_img = Image.open(BytesIO(old_img_bytes))
-
-    log.info(f"processing image")
-    # enlarge filled with bg
-    old_w, old_h = old_img.size
-    new_w = old_w
-    new_h = int(new_w * ratio)
-    bg_color = old_img.getpixel((10, 10))
-    new_img = Image.new("RGBA", size=(new_w, new_h), color=bg_color)
-    paste_anchor = (0, new_h - old_h)
-    new_img.paste(old_img, paste_anchor)
-
-    # write txt
-    log.info(f"done processing image")
-    #font_path = Path(current_app.root_path) / "static" / "fonts" / "Death_Note_Font_by_Karlibell22.ttf"
-    font_path = Path(current_app.root_path) / "static" / "fonts" / "AbrilFatface-Regular.ttf"
-    font = ImageFont.truetype(str(font_path), 250)
-    txt = Image.new('RGBA', new_img.size, (255,255,255,0))
-    drawer = ImageDraw.Draw(new_img)    
-    drawer.text((new_w // 2, (new_h - old_h) // 2), 
-                collection_info["title"],
-                fill=(0, 0, 0, 255), font=font, anchor="mm")
-    new_img = Image.alpha_composite(new_img, txt)
-
-    log.info(f"done processing image")
-
-    log.info(f"saving image")
-    cache_dir = "/tmp"
-    new_img_static_path_rel = f"wallpaper/cache/{today()}/{slug}/{idx}.png" # the relative path of the image under static
-    new_img_path = Path(cache_dir) / "static" / new_img_static_path_rel
-    #rm_old(Path(cache_dir) / "static")
-
-    new_img_path.parent.mkdir(parents=True, exist_ok=True)
-    new_img.save(str(new_img_path))
-    log.info(f"done saving image")
-
-    #new_img_url = url_for("static", filename=new_img_static_path_rel)
-    new_img_url = new_img_path
-
+    ratio = f"{h}by{w}"
+    new_img_url = url_for("wallpaper_img", slug=slug, idx=idx, ratio=ratio)
     return render_template("wallpaper_out.html", 
-                           old_img_url=old_img_url, 
                            new_img_url=new_img_url,
-                           #bg_color="rgb(" + ",".join([str(_) for _ in page_bg_color[:3]]) + ")"
-                           bg_color="rgb(0,0,0)",
-                           )
-
+                           bg_color="rgb(0,0,0)")
 
 
 if __name__ == '__main__':
